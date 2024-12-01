0d39cd55439ce0cec453ad57d72142c3407b2024
CaptainHook <boykostudyingkpi@gmail.com>
2024-12-01 20:47:45 +0200

    Add interfaces for repositories

---
+++ src/Blockchain.Data/Interfaces/IRemovable.cs
@@ -0,0 +1,7 @@
+namespace Blockchain.Data.Interfaces;
+
+public interface IRemovable<T> where T : class
+{
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entities);
+}
---
+++ src/Blockchain.Data/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+
+namespace Blockchain.Data.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task AddAsync(T entity);
+}
